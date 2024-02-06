d895dd87796d4c682b9aab3d525fd32654d64b7d
Lucie Daeye <lucie.daeye@digitalservice.bund.de>
2024-02-06 14:19:32 +0100

    Add test for hello world controller

---
+++ src/integrationTest/kotlin/de/bund/digitalservice/template/HelloWorldIntegrationTest.kt
@@ -0,0 +1,17 @@
+package de.bund.digitalservice.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HelloWorldIntegrationTest(
+    @Autowired val webTestClient: WebTestClient,
+) {
+    @Test
+    fun `should expose hello world`() {
+        webTestClient.get().uri("/").exchange().expectStatus().isOk()
+    }
+}
